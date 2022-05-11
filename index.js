e0db02cf628ee544229b820296424aefc4d538b3
Pete Skelly <peteskelly@gmail.com>
2022-05-11 09:20:41 -0400

    minor test formatting

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-console.log('test');
-
 // index.js
 const path = require('path')
 const fs = require('fs')
@@ -7,14 +5,14 @@ const fs = require('fs')
 const configFilePath = path.resolve(__dirname, './.github/workflows/pr-title-checker-config.json');
 const testsFilePath = path.resolve(__dirname, './tests.json');
 
-console.log(configFilePath);
-console.log(testsFilePath);
+console.log('\x1b[33mCONFIG:\x1b[0m:  ' + configFilePath);
+console.log('\x1b[33mTESTS:\x1b[0m:  ' + testsFilePath);
 
 let config = require(configFilePath);
 let tests = require(testsFilePath);
 
-console.log(config);
-console.log(tests);
+// console.log(config);
+// console.log(tests);
 
 let { CHECKS, LABEL, MESSAGES } = config;
 
